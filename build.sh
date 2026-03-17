76d7241b19fb77a52092fe82b61d2336df19c280
Grupo2 <grupo2@test.com>
2026-03-17 15:55:24 -0300

    Debug build: verificar INSTALLED_APPS y DB

--- build.sh
+++ build.sh
@@ -4,9 +4,16 @@ set -o errexit
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# Debug: verificar que Django carga
-python -c "import django; print(f'Django {django.VERSION} OK')"
-python manage.py help 2>&1 | head -5
+# Debug
+python -c "
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ferrelonstock.settings')
+import django
+django.setup()
+from django.conf import settings
+print('APPS:', settings.INSTALLED_APPS)
+print('DB:', settings.DATABASES['default']['ENGINE'])
+"
 
 python manage.py collectstatic --noinput
 python manage.py migrate
