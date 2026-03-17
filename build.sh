9ff207fb48cb82962b85e1fa45c849a70520e4c8
Grupo2 <grupo2@test.com>
2026-03-17 15:48:45 -0300

    Debug build: verificar que Django carga

--- build.sh
+++ build.sh
@@ -3,5 +3,10 @@ set -o errexit
 
 pip install --upgrade pip
 pip install -r requirements.txt
+
+# Debug: verificar que Django carga
+python -c "import django; print(f'Django {django.VERSION} OK')"
+python manage.py help 2>&1 | head -5
+
 python manage.py collectstatic --noinput
 python manage.py migrate
